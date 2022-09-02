21bf378fb1fe31655c81a9036473c6ce0e4ec35c
Oksana Kaledina <kaledinaoa@gmail.com>
2022-09-02 17:46:28 +0400

    Initial Commit

---
+++ Exitek/ExitekApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExitekApp.swift
+//  Exitek
+//
+//  Created by Oksana Kaledina on 02.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExitekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
